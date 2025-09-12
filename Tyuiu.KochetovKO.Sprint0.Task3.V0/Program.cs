ba1b34404d965761d304490317f3af04bce786a2
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-09-13 00:10:36 +0500

    Task3

--- Tyuiu.KochetovKO.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.KochetovKO.Sprint0.Task3.V0/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Tyuiu.KochetovKO.Sprint0.Task3.V0.Lib;
 namespace Tyuiu.KochetovKO.Sprint0.Task3.V0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.WriteLine();
         }
     }
 }
