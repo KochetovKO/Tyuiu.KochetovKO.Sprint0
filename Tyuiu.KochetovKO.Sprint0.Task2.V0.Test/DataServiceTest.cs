52359fd0f81bee51200f7cc0324cda7c040b5fa1
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-09-12 22:35:23 +0500

    Task2

---
+++ Tyuiu.KochetovKO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KochetovKO.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KochetovKO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Кирилл";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("привет, Кирилл", res);
+        }
+    }
+}
